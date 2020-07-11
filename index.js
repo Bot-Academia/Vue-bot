96bc5286ff4965fd6c0f892c1f802518647013b1
Vinayak Sharma <54861487+vinayaksh42@users.noreply.github.com>
2020-07-11 23:55:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const T = new Twit({
 
 // start stream and track tweets
 
-const stream = T.stream('statuses/filter', {track: ['#vue','#vuejs','#vue.js','#Vue.js','#Vuejs','#vuex','#vuetify']});
+const stream = T.stream('statuses/filter', {track: ['#vue','#vuejs','#vue.js','#Vue.js','#Vuejs','#vuex','#vuetify','#Vue']});
 
 
 // use this to log errors from requests
